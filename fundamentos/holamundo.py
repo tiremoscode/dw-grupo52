cc81677519ada24c47756466404b3bce7a2e7c84
Alberto Luebbert M. <albertoluebbert@ideashappy.com>
2024-09-17 12:13:44 -0600

    :sparkles: Clase Fundamentos de Programación

---
+++ fundamentos/holamundo.py
@@ -0,0 +1,6 @@
+def helloWorld():
+    print("Hola mundo - Grupo 52")
+    multiplica = 25 * 96
+    print("25 * 96 = " + str(multiplica))
+    
+helloWorld()
---
+++ fundamentos/script.js
@@ -0,0 +1,5 @@
+function helloWorld() {
+    console.log('Hola Mundo - Grupo 52');
+    var multiplica = 25 * 96;
+    console.log('25 * 96 = ' + multiplica);
+}
