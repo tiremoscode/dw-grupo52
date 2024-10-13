4d4310d90ab3e6dd5bb33c99c60e4fbacf94b9b4
Moni Santuario <ocira.mon@gmail.com>
2024-10-12 22:08:23 -0600

    Tarea de moni garcia corregida

--- practicas/js01/tarea-monigarcia.js
+++ practicas/js01/tarea-monigarcia.js
@@ -12,21 +12,15 @@
     Ejemplo: Suma de 2 + 2;
     */
 
-    function calculadora(valor1, valor2, operacionsuma) {
-        var valor1 = 5;
-        var valor2 = 2;
-        var operacionsuma = valor1 + valor2;
-        console.log("la suma es igual a " + operacionsuma);
-
-       var operacionresta = valor1 - valor2;
-       console.log("la resta es igual a " + operacionresta);
-
-       var operacionmulti = valor1 * valor2;
-       console.log("la multiplicación es igual a " + operacionmulti);
-
-       var operaciondiv = valor1 / valor2;
-       console.log("la division es igual a " + operaciondiv);
+    function calculadora(valor1, valor2, operacion) {
+        return {
+            suma: valor1 + valor2,
+            resta: valor1 - valor2,
+            multiplicacion: valor1 * valor2,
+            division: valor1 / valor2,}
+    
+       
+    
 
     }
-    
-    calculadora(2, 2, 'suma');
+    console.log( calculadora(3,2));
