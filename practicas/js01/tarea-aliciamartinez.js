55a23743b3148c448877e50dd423131a536bc1fc
Alicia Martinez <aliciamosqueda520@gmail.com>
2024-10-14 00:17:43 -0600

    Agregue modificaciones al código de la calculadora

--- practicas/js01/tarea-aliciamartinez.js
+++ practicas/js01/tarea-aliciamartinez.js
@@ -12,8 +12,40 @@
     Ejemplo: Suma de 2 + 2;
     */
 
-    function calculadora(valor1, valor2, operacion) {
-        // ToDo
-    }
-    
-    calculadora(2, 2, 'suma');
+    let respuesta = "";
+
+    do{
+        console.log("1.- Suma");
+        console.log("2.- Resta");
+        console.log("3.- Multiplicación");
+        console.log("4.- División");
+        console.log("5.- Salir");
+        respuesta = interface.question("¿Qué operación desea realizar?, escriba el número: ");
+
+        if(respuesta != "5"){
+            const primerNumero = interface.question("Agregue el primer número: ");
+            const segundoNumero = interface.question("Agregue el segundo número: ");
+
+            switch(respuesta){
+                case "1":
+                    var resultado = parseInt(primerNumero) + parseInt(segundoNumero);
+                    console.log("El resultado es: "+resultado);
+                    break;
+                    case "2":
+                        var resultado = parseInt(primerNumero) - parseInt(segundoNumero);
+                        console.log("El resultado es: "+resultado);
+                        break;
+                        case "3":
+                            var resultado = parseInt(primerNumero) * parseInt(segundoNumero);
+                            console.log("El resultado es: "+resultado);
+                            break;
+                            case "4":
+                                var resultado = parseInt(primerNumero) / parseInt(segundoNumero);
+                                console.log("El resultado es: "+resultado);
+                                break;
+                                default:
+                                break;
+            }
+            interface.question("Presione enter para continuar");
+        }
+    }while(respuesta != "5");  
