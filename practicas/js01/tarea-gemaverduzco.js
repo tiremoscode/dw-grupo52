4f39cdf06b10b9c03b4d45fe3e010dac3e7c8292
gema verduzco <102296g@gmail.com>
2024-10-16 10:10:34 -0600

    agregué mi tarea js

--- practicas/js01/tarea-gemaverduzco.js
+++ practicas/js01/tarea-gemaverduzco.js
@@ -17,3 +17,15 @@ function calculadora(valor1, valor2, operacion) {
 }
 
 calculadora(2, 2, 'suma');
+
+let operador = prompt('que opercion desea hacer: 1- Sumar || 2- Restar');
+let numero1 = Number(prompt('Ingrese el primer numero'))
+let numero2 = Number(prompt('Ingrese el segundo numero'))
+
+if(operador == 1){
+    total= numero1+numero2
+    alert('El resultado de la suma es de'+total)
+}else if(operador == 2){
+    total = numero1-numero2
+    alert('El resultado de la resta es de'+total)
+}
