d577aee6bf48d865a6f0870c2eda0d59fbbf9a63
Alberto Luebbert M. <albertoluebbert@ideashappy.com>
2024-10-16 10:18:15 -0600

    :sparkles: Restaure el archivo tarea.js

---
+++ practicas/js01/tarea.js
@@ -0,0 +1,19 @@
+/* Instrucciones de la tarea */
+
+/* Calculadora 
+    Suma
+    Resta
+    Multiplicación
+    División 
+    
+    Crear una función que a través de 3 parámetros
+    permita hacer la operación solicitada
+
+    Ejemplo: Suma de 2 + 2;
+    */
+
+    function calculadora(valor1, valor2, operacion) {
+        // ToDo
+    }
+    
+    calculadora(2, 2, 'suma');
