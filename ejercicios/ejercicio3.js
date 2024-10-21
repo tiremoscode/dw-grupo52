4473b22d59063eb3af3f870edd2534f3b1f0f9f0
Lizbeth Marcial <codedgray@gmail.com>
2024-10-21 12:18:18 -0600

    Agregué en el ejercicio3 mi saludo en japonés

--- ejercicios/ejercicio3.js
+++ ejercicios/ejercicio3.js
@@ -11,7 +11,12 @@ function holaGrupo52f(nombre) {
     console.log('Bienvenue ' + nombre);
 }
 
+function holaGrupo52m(nombre){
+    console.log('Konnichiwa ' + nombre);
+    
+}
+
 holaGrupo52s('Lizbeth');
 holaGrupo52e('Arantxa');
 holaGrupo52f('Jania');
-
+holaGrupo52m('Lizbeth Marcial');
