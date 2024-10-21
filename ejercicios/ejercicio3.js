b2619a74f3ea7d13b736988e1989f83bd8f3c412
ClaudiaS <gotica166@gmail.com>
2024-10-20 18:44:06 -0600

    Agregue un saludos en japones junto con mi nombre

--- ejercicios/ejercicio3.js
+++ ejercicios/ejercicio3.js
@@ -10,8 +10,12 @@ function holaGrupo52e(nombre) {
 function holaGrupo52f(nombre) {
     console.log('Bienvenue ' + nombre);
 }
+function holaGrupo52c(nombre){
+    console.log('konnichiwa' + nombre);
+}
 
 holaGrupo52s('Lizbeth');
 holaGrupo52e('Arantxa');
 holaGrupo52f('Jania');
+holaGrupo52c('Claudia');
 
