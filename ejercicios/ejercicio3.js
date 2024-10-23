58b1179174e1a6e564e7c6311d7c3cfbe952354a
Alberto Luebbert M. <alberto.luebbert@internationalsos.com>
2024-10-23 12:44:18 -0600

    :fire: Actualización rutas archivos

--- ejercicios/ejercicio3.js
+++ ejercicios/ejercicio3.js
@@ -3,36 +3,38 @@ function holaGrupo52s(nombre) {
 }
 
 function holaGrupo52e(nombre) {
-    console.log('Welcome' + nombre);
+    console.log('Welcome ' + nombre);
 }
 
 function holaGrupo52f(nombre) {
     console.log('Bienvenue ' + nombre);
 }
-function holaGrupo52c(nombre){
-    console.log('konnichiwa' + nombre);
+function holaGrupo52c(nombre) {
+    console.log('konnichiwa ' + nombre);
 }
 
 function holaGrupo52m(nombre) {
-    console.log('Hello, welcome!' + nombre);
+    console.log('Hello, welcome! ' + nombre);
 }
-
 
 function holaGrupo52w(nombre) {
     console.log('Bem-vindo ' + nombre);
 }
 
-function holaGrupo52g(nombre){
+function holaGrupo52g(nombre) {
     console.log('Willkommen ' + nombre);
 }
 
 function holaGrupo52I(nombre) {
-    console.log('Benvenuto' + nombre);
+    console.log('Benvenuto ' + nombre);
 }
 
-function holaGrupo52j(nombre){
+function holaGrupo52j(nombre) {
     console.log('Konnichiwa ' + nombre);
-    
+}
+
+function holaGrupo52English(nombre) {
+    console.log('Glad to have you here ' + nombre);
 }
 
 holaGrupo52s('1-Lizbeth');
@@ -45,3 +47,4 @@ holaGrupo52g('6-Lizbeth Marcial');
 holaGrupo52w('7- Andrea Ortiz');
 holaGrupo52c('8- Claudia Sanchez');
 holaGrupo52m('9- Marisol Cordova');
+holaGrupo52English('10- Lilian');
