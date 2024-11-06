42c3828837db52e30c5d517caeb515d1677f80b2
Marisol Cordova <solcordova_star@hotmail.com>
2024-11-06 14:21:07 -0600

    Agregue una nueva rama, una nueva carpeta y un nuevo archivo con terminacion 03 que hace referencia a la tarea de objetos

---
+++ practicas/js03/js03-marisolc/objetos03.js
@@ -0,0 +1,100 @@
+//integrantes de mi familia//
+
+const integrante1 = {
+    nombre: 'Ricardo Gomez Gomez',
+    puesto: 'papa',
+    edad: 46,
+    ocupacion: 'taxista y vendedor de terrenos',
+    nacimiento: '28 de junio de 1978',
+    comidasFavoritas: {
+        'pozole': true,
+        'mole': true,
+        'caldo_de_pollo_de_rancho': true,
+        'caldoTlalpeño': false,
+        'asadoColeto': true,
+        'frijolesCharros': false,
+        'caldo_de_mondongo': false, 
+    }
+}
+
+const integrante2 = {
+    nombre: 'Teresa Cordova Bautista',
+    puesto: 'mama',
+    edad: 42,
+    ocupacion: 'ama de casa',
+    nacimiento: '02 de diciembre de 1982',
+    comidasFavoritas: {
+        'pozole': true,
+        'mole': true,
+        'caldo_de_pollo_de_rancho': true,
+        'caldoTlalpeño': true,
+        'asadoColeto': true,
+        'frijolesCharros': false,
+        'caldo_de_mondongo': true, 
+    }
+}
+
+const integrante3 = {
+    nombre: 'Yareni Marisol Gomez Cordova',
+    puesto: 'hija mayor (yo)',
+    edad: 24,
+    ocupacion: 'eterna aprendiz, hostess, creadora de proyectos productivos',
+    nacimiento: '06 de junio del 2000',
+    comidasFavoritas: {
+        'pozole': true,
+        'mole': false,
+        'caldo_de_pollo_de_rancho': false,
+        'caldoTlalpeño': true,
+        'asadoColeto': true,
+        'frijolesCharros': true,
+        'caldo_de_mondongo': true, 
+    }
+}
+
+const integrante4 = {
+    nombre: 'Jazmin Gomez Cordova',
+    puesto: 'hija menor',
+    edad: 18,
+    profesion: 'estudiante de derecho',
+    nacimiento: '06 de mayo del 2006',
+    comidasFavoritas: {
+        'pozole': true,
+        'mole': true,
+        'caldo_de_pollo_de_rancho': false,
+        'caldoTlalpeño': true,
+        'asadoColeto': true,
+        'frijolesCharros': false,
+        'caldo_de_mondongo': false, 
+    }
+}
+
+const integrante5 = {
+    nombre: 'Fernanda Gomez Cordova',
+    puesto: 'hija pequeña',
+    edad: 14,
+    profesion: 'estudiante de secundaria',
+    nacimiento: '01 de agosto de 2010',
+    comidasFavoritas: {
+        'pozole': true,
+        'mole': true,
+        'caldo_de_pollo_de_rancho': false,
+        'caldoTlalpeño': true,
+        'asadoColeto': false,
+        'frijolesCharros': false,
+        'caldo_de_mondongo': false, 
+    }
+}
+
+
+//imprimir propiedades
+//console.log(integrante4);
+console.log(integrante5);
+
+//Modificamos alguna que otra propiedad
+integrante5.puesto = "hija mayor";
+
+console.log(integrante5)
+
+
+//
+
