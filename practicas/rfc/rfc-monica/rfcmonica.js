0199af2445f94287c0ea8b6d5d6706cc4177e452
Moni Santuario <ocira.mon@gmail.com>
2024-11-10 20:25:35 -0600

    Añadi mi tarea en la carpeta de parcticas/rfc/rfc-monica

---
+++ practicas/rfc/rfc-monica/rfcmonica.js
@@ -0,0 +1,15 @@
+function generarrfc () {
+    var nombre = document.getElementById('nombre').value.toUpperCase();
+    var paterno = document.getElementById('paterno').value.toUpperCase();
+    var materno = document.getElementById('materno').value.toUpperCase();
+    var cumpleanios = document.getElementById('cumpleanios').value;
+    
+    
+    var rfcgenerado = nombre.substring(0, 1) + paterno.substring(0, 2) + materno.substring(0, 1) + cumpleanios.replace(/-/g, '').substring
+    (2) + 'XXX' ;
+
+    document.getElementById('rfc').textContent = 'RFC generado: ' + rfcgenerado;
+    //const rfc = document.getElementById('resultado');
+    //resultado.innerHTML = `resultado' ${rfcgenerado}`;
+
+}
